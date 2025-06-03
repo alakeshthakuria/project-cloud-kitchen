3182c17fd9d0680be7cbc1d63ed2772b31fb8a6b
alakeshthakuria <alakeshthakuria>
2025-06-03 14:54:32 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Simple script to show smooth scrolling
+document.querySelectorAll('nav a').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      target.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
