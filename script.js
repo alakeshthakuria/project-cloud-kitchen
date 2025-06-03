2e4476a9de869e75c4117f43618ce1dd42b75cba
alakeshthakuria <alakeshthakuria>
2025-06-03 17:26:45 +0530

    updated layout and feature

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-// Simple script to show smooth scrolling
-document.querySelectorAll('nav a').forEach(anchor => {
-  anchor.addEventListener('click', function(e) {
-    e.preventDefault();
-    const target = document.querySelector(this.getAttribute('href'));
-    if (target) {
-      target.scrollIntoView({ behavior: 'smooth' });
-    }
-  });
-});
+function toggleIngredients(dishElement) {
+  const ingredients = dishElement.querySelector(".ingredients");
+  ingredients.classList.toggle("hidden");
+}
